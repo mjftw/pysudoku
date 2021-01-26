a3051939d2480ece33c1714bcce6949af8acbe8a
Merlin Webster <mjftwebster@gmail.com>
2021-01-26 20:50:17 +0000

    Add example to API docs

--- src/pysudoku/web/models.py
+++ src/pysudoku/web/models.py
@@ -4,3 +4,20 @@ from pydantic import BaseModel
 
 class BoardModel(BaseModel):
     board: List[List[int]]
+
+    class Config:
+        schema_extra = {
+            'example': {
+                'board': [
+                    [0,0,0,0,0,0,0,0,7],
+                    [0,0,0,0,0,0,0,3,4],
+                    [0,0,7,0,0,3,0,0,0],
+                    [0,8,0,4,0,0,0,0,9],
+                    [2,4,1,7,5,0,0,0,3],
+                    [0,5,8,0,0,4,0,0,0],
+                    [0,1,9,8,0,0,7,0,2],
+                    [7,0,3,0,1,0,0,5,0]
+                ]
+            }
+        }
+
